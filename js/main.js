8546951f82139b4354b03203ac46f655ba6bb5ca
leoses03 <135790417+leoses03@users.noreply.github.com>
2023-06-07 14:19:41 -0400

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-async function searchArticles(term) {
-    try {
-      const response = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=${term}`);
-      const articles = response.data.query.search;
-      // Aquí puedes manipular los datos de los artículos obtenidos y actualizar la interfaz de usuario
-    } catch (error) {
-      console.error(error);
-    }
-  }  
