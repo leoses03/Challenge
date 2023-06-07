023d81ec8d183015548d88ce105a97aca5f989ef
leoses03 <135790417+leoses03@users.noreply.github.com>
2023-06-07 14:23:01 -0400

    Subí los archivos del proyecto

---
+++ script.js
@@ -0,0 +1,112 @@
+const submitButton = document.querySelector('#submit');
+const input = document.querySelector('#input');
+const errorSpan = document.querySelector('#error');
+const resultsContainer = document.querySelector('#results');
+
+const endpoint = 'https://es.wikipedia.org/w/api.php?';
+const params = {
+    origin: '*',
+    format: 'json',
+    action: 'query',
+    prop: 'extracts',
+    exchars: 250,
+    exintro: true,
+    explaintext: true,
+    generator: 'search',
+    gsrlimit: 40,
+    gsrsearch: '',
+};
+
+const disableUi = () => {
+    input.disabled = true;
+    submitButton.disabled = true;
+};
+
+const enableUi = () => {
+    input.disabled = false;
+    submitButton.disabled = false;
+};
+
+const clearPreviousResults = () => {
+    resultsContainer.innerHTML = '';
+    errorSpan.innerHTML = '';
+};
+
+const isInputEmpty = input => {
+    if (!input || input === '') return true;
+    return false;
+};
+
+const showError = error => {
+    errorSpan.innerHTML = `🚨 ${error} 🚨`;
+};
+
+const showResults = results => {
+    results.forEach(result => {
+        resultsContainer.innerHTML += `
+        <div class="results__item">
+            <a href="https://es.wikipedia.org/?curid=${result.pageId}" target="_blank" class="card animated bounceInUp">
+                <h2 class="results__item__title">${result.title}</h2>
+                <p class="results__item__intro">${result.intro}</p>
+            </a>
+        </div>
+    `;
+    });
+};
+
+const gatherData = pages => {
+    const results = Object.values(pages).map(page => ({
+        pageId: page.pageid,
+        title: page.title,
+        intro: page.extract,
+    }));
+
+    showResults(results);
+};
+
+const getData = async () => {
+    const userInput = input.value;
+    if (isInputEmpty(userInput)) return;
+
+    params.gsrsearch = userInput;
+    clearPreviousResults();
+    disableUi();
+
+    try {
+        const { data } = await axios.get(endpoint, { params });
+
+        if (data.error) throw new Error(data.error.info);
+        gatherData(data.query.pages);
+    } catch (error) {
+        showError(error);
+    } finally {
+        enableUi();
+    }
+};
+
+const handleKeyEvent = e => {
+    if (e.key === 'Enter') {
+        getData();
+    }
+};
+
+let debounceTimer;
+
+const handleInputEvent = () => {
+    clearPreviousResults();
+    const userInput = input.value;
+    if (isInputEmpty(userInput)) return;
+
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => {
+        params.gsrsearch = userInput;
+        getData();
+    }, 1000);
+};
+
+const registerEventHandlers = () => {
+    input.addEventListener('input', handleInputEvent);
+    submitButton.addEventListener('click', getData);
+};
+
+registerEventHandlers();
